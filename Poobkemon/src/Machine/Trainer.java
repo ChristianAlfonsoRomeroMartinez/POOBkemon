11f1c371365da606ec4e5780d740b34178938628
Christian Alfonso Romero Martinez <christian.romero-m@mail.escuelaing.edu.co>
2025-05-11 18:33:04 -0500

    machine new

--- Poobkemon/src/Machine/Trainer.java
+++
@@ -1,9 +0,0 @@
-package Machine;
-
-import java.util.List;
-
-public interface Trainer {
-    Pokemon elegirPokemon(List<Pokemon> pokemons);
-    Attack elegirAtaque(Pokemon pokemonAtacante, Pokemon pokemonDefensor);
-    void tomarDecisionEstrategica(BattleArena arena);
-}
